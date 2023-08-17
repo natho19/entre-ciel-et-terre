922b6aa6ceb4ab83187a3e52fde8ff1ea03232c6
Nathan Akpawu <nakpawu@gmail.com>
2023-08-17 17:31:38 +0000

    single page

---
+++ single.php
@@ -0,0 +1,127 @@
+<?php get_header(); ?>
+
+<section id="content_main" class="clearfix jl_spost">
+    <div class="container">
+        <div class="row main_content">
+            <div class="col-md-8  loop-large-post" id="content">
+                <div class="widget_container content_page">
+                    <!-- start post -->
+                    <div class="post-2838 post type-post status-publish format-standard has-post-thumbnail hentry category-sports tag-gaming" id="post-2838">
+                        <div class="single_section_content box blog_large_post_style">
+                            <div class="jl_single_style2">
+                                <div class="single_post_entry_content single_bellow_left_align jl_top_single_title jl_top_title_feature">
+                                    <h1 class="single_post_title_main">Lorem ipsum dolor</h1>
+                                    <span class="jl_post_meta">
+                                        <span class="post-date"><i class="jli-pen"></i>July 24, 2016</span>
+                                        <span class="post-read-time"><i class="jli-watch-2"></i>2 Mins</span>
+                                    </span>
+                                </div>
+                                <div class="single_content_header jl_single_feature_below">
+                                    <div class="image-post-thumb jlsingle-title-above">
+                                        <img src="<?= ECT_IMG_URL . '900x600.png'; ?>" class="attachment-sprasa_feature_large size-sprasa_feature_large wp-post-image" alt="" loading="lazy">
+                                        <!-- <iframe width="560" height="315" src="https://www.youtube.com/embed/E1hG6_muvK4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="post_content_w">
+                                <div class="post_sw">
+                                    <div class="post_s">
+                                        <div class="jl_single_share_wrapper jl_clear_at">
+                                            <ul class="single_post_share_icon_post">
+                                                <li class="single_post_share_facebook">
+                                                    <a href="#" target="_blank"><i class="jli-facebook"></i></a>
+                                                </li>
+                                                <li class="single_post_share_twitter">
+                                                    <a href="#" target="_blank"><i class="jli-twitter"></i></a>
+                                                </li>
+                                                <li class="single_post_share_linkedin">
+                                                    <a href="#" target="_blank"><i class="jli-linkedin"></i></a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="post_content jl_content">
+                                    <p>Mauris mattis auctor cursus. Phasellus tellus tellus, imperdiet ut imperdiet eu, iaculis a sem Donec vehicula luctus nunc <a href="#">in laoreet Aliquam erat volutpat</a>. Suspendisse vulputate porttitor condimentum. Proin viverra orci a leo suscipit placerat. Sed feugiat posuere semper. Cras vitae mi erat, posuere mollis arcu. Pellentesque iaculis gravida nulla ac hendrerit. Vestibulum faucibus neque at lacus tristique eu ultrices ipsum mollis. Phasellus venenatis, lacus in malesuada pellentesque, nisl ipsum faucibus velit, et eleifend velit nulla a mi. Praesent pharetra semper purus, a vehicula massa interdum in <a href="#">Nulla a magna at diam consequat</a> semper eu vitae elit. In hac habitasse platea dictumst.</p>
+                                    <p>Phasellus tellus tellus, imperdiet ut imperdiet eu, iaculis a sem. Suspendisse vulputate porttitor condimentum. <a href="#">Proin viverra orci a leo suscipit placerat</a>. Sed feugiat posuere semper. Cras vitae mi erat, posuere mollis arcu. Pellentesque iaculis gravida nulla ac hendrerit. Vestibulum faucibus neque at lacus tristique eu ultrices ipsum mollis.&nbsp;</p>
+                                    <h3>Find the best tour for this summer</h3>
+                                    <p>Pellentesque iaculis gravida <a href="#">nulla ac hendrerit</a>. Vestibulum faucibus neque at lacus tristique eu ultrices ipsum mollis. Phasellus venenatis, lacus in malesuada pellentesque, nisl ipsum faucibus velit, et eleifend velit nulla a mi. Praesent pharetra semper purus, a vehicula massa interdum in.</p>
+                                    <ul>
+                                        <li>Nisl ipsum faucibus velit, et eleifend velit nulla</li>
+                                        <li>Venenatis&nbsp;<em>sint occaecat</em>&nbsp;cupidatat non proident.</li>
+                                        <li>Praesent pharetra semper purus&nbsp;accusamus et iusto&nbsp;odio.</li>
+                                        <li>Cras vitae mi erat, posuere mollis arcu</li>
+                                    </ul>
+                                    <p>Pellentesque iaculis gravida nulla ac hendrerit. Vestibulum faucibus neque at lacus tristique eu ultrices ipsum mollis. Phasellus venenatis, lacus in malesuada pellentesque, <a href="#">nisl ipsum faucibus velit</a>, et eleifend velit nulla a mi. Praesent pharetra semper purus, a vehicula massa interdum in.</p>
+                                </div>
+                            </div>
+                            <div class="clearfix"></div>
+                            <div class="related-posts">
+                                <h4>Articles similaires</h4>
+                                <div class="single_related_post">												
+                                    <div class="jl_m_right jl_m_list jl_m_img">
+                                        <div class="jl_m_right_w">
+                                            <div class="jl_m_right_img jl_radus_e">
+                                                <a href="http://localhost/entrecieletterre/lorem-ipsum-dolor/">
+                                                    <img src="<?= ECT_IMG_URL . '900x600.png'; ?>" class="attachment-sprasa_slider_grid_small size-sprasa_slider_grid_small wp-post-image" alt="" loading="lazy">
+                                                </a>
+                                            </div>
+                                            <div class="jl_m_right_content">
+                                                <h2 class="entry-title"> <a href="#" tabindex="-1">How to make your life routine more fun and eco-friendly</a></h2>
+                                                <span class="jl_post_meta">
+                                                    <span class="post-date"><i class="jli-pen"></i>July 10, 2019</span>
+                                                    <span class="post-read-time"><i class="jli-watch-2"></i>2 Mins</span>
+                                                </span>
+                                                <p>Mauris mattis auctor cursus. Phasellus tellus tellus, imperdiet ut imperdiet eu, iaculis a sem Donec vehicula luctus nunc...</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="jl_m_right jl_m_list jl_m_img">
+                                        <div class="jl_m_right_w">
+                                            <div class="jl_m_right_img jl_radus_e">
+                                                <a href="http://localhost/entrecieletterre/lorem-ipsum-dolor/">
+                                                    <img src="<?= ECT_IMG_URL . '900x600.png'; ?>" class="attachment-sprasa_slider_grid_small size-sprasa_slider_grid_small wp-post-image" alt="" loading="lazy">
+                                                </a>
+                                            </div>
+                                            <div class="jl_m_right_content">
+                                                <h2 class="entry-title"> <a href="#" tabindex="-1">How to make your life routine more fun and eco-friendly</a></h2>
+                                                <span class="jl_post_meta">
+                                                    <span class="post-date"><i class="jli-pen"></i>July 10, 2019</span>
+                                                    <span class="post-read-time"><i class="jli-watch-2"></i>2 Mins</span>
+                                                </span>
+                                                <p>Mauris mattis auctor cursus. Phasellus tellus tellus, imperdiet ut imperdiet eu, iaculis a sem Donec vehicula luctus nunc...</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="jl_m_right jl_m_list jl_m_img">
+                                        <div class="jl_m_right_w">
+                                            <div class="jl_m_right_img jl_radus_e">
+                                                <a href="http://localhost/entrecieletterre/lorem-ipsum-dolor/">
+                                                    <img src="<?= ECT_IMG_URL . '900x600.png'; ?>" class="attachment-sprasa_slider_grid_small size-sprasa_slider_grid_small wp-post-image" alt="" loading="lazy">
+                                                </a>
+                                            </div>
+                                            <div class="jl_m_right_content">
+                                                <h2 class="entry-title"> <a href="#" tabindex="-1">How to make your life routine more fun and eco-friendly</a></h2>
+                                                <span class="jl_post_meta">
+                                                    <span class="post-date"><i class="jli-pen"></i>July 10, 2019</span>
+                                                    <span class="post-read-time"><i class="jli-watch-2"></i>2 Mins</span>
+                                                </span>
+                                                <p>Mauris mattis auctor cursus. Phasellus tellus tellus, imperdiet ut imperdiet eu, iaculis a sem Donec vehicula luctus nunc...</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- #respond -->
+                        </div>
+                    </div>
+                    <!-- end post -->
+                    <div class="brack_space"></div>
+                </div>
+            </div>
+            <?php get_template_part('parts/page', 'sidebar'); ?>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
