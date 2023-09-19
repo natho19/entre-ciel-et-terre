66619c5161b9025091ccbffb474fd16aea067772
Nathan Akpawu <nakpawu@gmail.com>
2023-09-19 11:31:45 +0000

    sidebar

--- parts/page-sidebar.php
+++ parts/page-sidebar.php
@@ -1,5 +1,6 @@
 <div class="col-md-4" id="sidebar">
     <div class="jl_sidebar_w">
+        <!-- Publicité -->
         <div id="sprasa_ads300x250_widget-2" class="widget jellywp_ads300x250_widget">
             <div class="widget_jl_wrapper ads_widget_container">
                 <div class="widget-title">
@@ -7,108 +8,51 @@
                 </div>
                 <div class="advertisement">
                     <a href="#">
-                        <img src="<?= ECT_IMG_URL . '400x400.png' ?>" alt="Image">
+                        <img src="<?= ECT_IMG_URL . 'pub.png' ?>" alt="Publicité">
                     </a>
                 </div>
             </div>
         </div>
-        <!-- <div id="sprasa_widget_social_counter_c-2" class="widget jl-widget-social-counter">
-            <div class="widget-title">
-                <h2 class="jl_title_c">Resté connecté</h2>
-            </div>
-            <div class="jl_social_counter social_counter_wraper jl_count_style_2 social_counter_item>">
-                <div class="jlsocial-element jl-facebook jl_radus_e">
-                    <a target="_blank" href="https://facebook.com/envato" class="facebook" title="facebook"></a>
-                    <span class="jlsocial-element-left">
-                        <i class="jli-facebook" aria-hidden="true"></i>
-                        <span class="num-count">228.8k</span>
-                    </span>
-                    <span class="jlsocial-element-right">fans</span>
-                </div>
-                <div class="jlsocial-element jl-youtube jl_radus_e">
-                    <a target="_blank" href="https://www.youtube.com/user/envato " title="Youtube"></a>
-                    <span class="jlsocial-element-left">
-                        <i class="jli-youtube" aria-hidden="true"></i>
-                        <span class="num-count">65.5k</span>
-                    </span>
-                    <span class="jlsocial-element-right">subscribers</span>
-                </div>
-                <div class="jlsocial-element jl-vimeo jl_radus_e">
-                    <a target="_blank" href="https://vimeo.com/beeple" title="vimeo"></a>
-                    <span class="jlsocial-element-left">
-                        <i class="jli-vimeo" aria-hidden="true"></i>
-                        <span class="num-count">240</span>
-                    </span>
-                    <span class="jlsocial-element-right">subscribers</span>
-                </div>
-                <div class="jlsocial-element jl-pinterest jl_radus_e">
-                    <a target="_blank" href="https://pinterest.com/envato" class="pinterest" title="pinterest"></a>
-                    <span class="jlsocial-element-left">
-                        <i class="jli-pinterest" aria-hidden="true"></i>
-                        <span class="num-count">9.87k</span>
-                    </span>
-                    <span class="jlsocial-element-right">followers</span>
-                </div>
-            </div>
-        </div> -->
-        <div id="sprasa_recent_post_text_widget-9" class="widget post_list_widget">
-            <div class="widget_jl_wrapper">
-                <div class="ettitle">
-                    <div class="widget-title">
-                        <h2 class="jl_title_c">Derniers articles</h2>
-                    </div>
-                </div>
-                <div class="bt_post_widget">
-                    <div class="jl_m_right jl_sm_list jl_ml jl_clear_at">
-                        <div class="jl_m_right_w">
-                            <div class="jl_m_right_img jl_radus_e">
-                                <a href="#">
-                                    <img width="120" height="120" src="<?= ECT_IMG_URL . '200x200.png'; ?>" class="attachment-sprasa_small_feature size-sprasa_small_feature wp-post-image" alt="Image" loading="lazy">
-                                </a>
-                            </div>
-                            <div class="jl_m_right_content">
-                                <h2 class="entry-title"> <a href="#" tabindex="-1">This place really good place for reading</a></h2>
-                                <span class="jl_post_meta">
-                                    <span class="post-date"><i class="jli-pen"></i>July 24, 2016</span>
-                                    <span class="post-read-time"><i class="jli-watch-2"></i>2 Mins</span>
-                                </span>
-                            </div>
+
+        <!-- Derniers posts -->
+        <?php
+        $last_posts = ect_get_objects('post', 3);
+        if ($last_posts->have_posts()) : ?>
+            <div id="sprasa_recent_post_text_widget-9" class="widget post_list_widget">
+                <div class="widget_jl_wrapper">
+                    <div class="ettitle">
+                        <div class="widget-title">
+                            <h2 class="jl_title_c">Derniers articles</h2>
                         </div>
                     </div>
-                    <div class="jl_m_right jl_sm_list jl_ml jl_clear_at">
-                        <div class="jl_m_right_w">
-                            <div class="jl_m_right_img jl_radus_e">
-                                <a href="#">
-                                    <img width="120" height="120" src="<?= ECT_IMG_URL . '200x200.png'; ?>" class="attachment-sprasa_small_feature size-sprasa_small_feature wp-post-image" alt="Image" loading="lazy">
-                                </a>
+                    
+                    <div class="bt_post_widget">
+                        <?php while ($last_posts->have_posts()) : $last_posts->the_post(); ?>
+                            <div class="jl_m_right jl_sm_list jl_ml jl_clear_at">
+                                <div class="jl_m_right_w">
+                                    <div class="jl_m_right_img jl_radus_e">
+                                        <a href="<?php the_permalink(); ?>">
+                                            <img src="<?= has_post_thumbnail() ? get_the_post_thumbnail_url(get_the_ID(), 'thumbnail') : ECT_IMG_URL . '200x200.png'; ?>" class="attachment-sprasa_small_feature size-sprasa_small_feature wp-post-image" alt="<?= esc_attr(get_the_title()); ?>" loading="lazy">
+                                            <?php if (get_field('video_link')) : ?>
+                                                <span class="jl_post_type_icon"><i class="jli-youtube"></i></span>
+                                            <?php endif; ?>
+                                        </a>
+                                    </div>
+                                    <div class="jl_m_right_content">
+                                        <h2 class="entry-title"> <a href="<?php the_permalink(); ?>" tabindex="-1"><?php the_title(); ?></a></h2>
+                                        <span class="jl_post_meta">
+                                            <span class="post-date"><i class="jli-pen"></i><?= get_the_date('d/m/Y'); ?></span>
+                                            <?php if (get_field('read_time')) : ?>
+                                                <span class="post-read-time"><i class="jli-watch-2"></i><?php the_field('read_time'); ?> Mins</span>
+                                            <?php endif; ?>
+                                        </span>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="jl_m_right_content">
-                                <h2 class="entry-title"> <a href="#" tabindex="-1">This place really good place for reading</a></h2>
-                                <span class="jl_post_meta">
-                                    <span class="post-date"><i class="jli-pen"></i>July 24, 2016</span>
-                                    <span class="post-read-time"><i class="jli-watch-2"></i>2 Mins</span>
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="jl_m_right jl_sm_list jl_ml jl_clear_at">
-                        <div class="jl_m_right_w">
-                            <div class="jl_m_right_img jl_radus_e">
-                                <a href="#">
-                                    <img width="120" height="120" src="<?= ECT_IMG_URL . '200x200.png'; ?>" class="attachment-sprasa_small_feature size-sprasa_small_feature wp-post-image" alt="Image" loading="lazy">
-                                </a>
-                            </div>
-                            <div class="jl_m_right_content">
-                                <h2 class="entry-title"> <a href="#" tabindex="-1">This place really good place for reading</a></h2>
-                                <span class="jl_post_meta">
-                                    <span class="post-date"><i class="jli-pen"></i>July 24, 2016</span>
-                                    <span class="post-read-time"><i class="jli-watch-2"></i>2 Mins</span>
-                                </span>
-                            </div>
-                        </div>
+                        <?php endwhile; ?>
                     </div>
                 </div>
             </div>
-        </div>
+        <?php endif; wp_reset_postdata(); ?>
     </div>
 </div>
