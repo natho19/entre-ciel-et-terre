35c71f2b5e971fa40243e5d80c12dae6c78fc101
Nathan Akpawu <nakpawu@gmail.com>
2023-11-02 10:11:55 +0000

    clean and  minify CSS, JS files

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -2,14 +2,14 @@
 
 function ect_load_scripts() {
     // CSS
-    wp_enqueue_style('bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.css', [], null);
-    wp_enqueue_style('style', get_template_directory_uri() . '/assets/css/style.css', [], null);
-    wp_enqueue_style('responsive', get_template_directory_uri() . '/assets/css/responsive.css', [], null);
-    wp_enqueue_style('main', get_template_directory_uri() . '/assets/css/main.css', [], null);
+    wp_enqueue_style('bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.min.css', [], null);
+    wp_enqueue_style('style', get_template_directory_uri() . '/assets/css/style.min.css', [], null);
+    wp_enqueue_style('responsive', get_template_directory_uri() . '/assets/css/responsive.min.css', [], null);
+    wp_enqueue_style('main', get_template_directory_uri() . '/assets/css/main.min.css', [], null);
 
     // JS
-    wp_enqueue_script('fluidvids', get_template_directory_uri() . '/assets/js/fluidvids.js', ['jquery'], null, true);
-    wp_enqueue_script('slick', get_template_directory_uri() . '/assets/js/slick.js', ['jquery'], null, true);
-    wp_enqueue_script('custom', get_template_directory_uri() . '/assets/js/custom.js', ['jquery'], null, true);
+    wp_enqueue_script('fluidvids', get_template_directory_uri() . '/assets/js/fluidvids.min.js', ['jquery'], null, true);
+    wp_enqueue_script('slick', get_template_directory_uri() . '/assets/js/slick.min.js', ['jquery'], null, true);
+    wp_enqueue_script('custom', get_template_directory_uri() . '/assets/js/custom.min.js', ['jquery'], null, true);
 }
 add_action('wp_enqueue_scripts', 'ect_load_scripts');
